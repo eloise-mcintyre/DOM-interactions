2acc06e3c62b3a48f9dff737393cb581cac2a03c
eloise-mcintyre <eloise.mcintyre2@outlook.com>
2019-09-22 20:10:58 +1200

    make the dots dissapear when double clicked

--- game.js
+++ game.js
@@ -13,6 +13,7 @@ function bindEventListeners (dots) {
     // The first one is provided for you
     dots[i].addEventListener('contextmenu', makeGreen)
     dots[i].addEventListener('click', makeBlue)
+    dots[i].addEventListener('dblclick', hide)
   }
 }
 
@@ -27,8 +28,12 @@ function makeBlue (evt) {
   evt.target.classList.toggle('blue')
   updateCounts()
 }
-// CREATE FUNCTION hide HERE
 
+// CREATE FUNCTION hide HERE
+function hide (evt) {
+  evt.target.classList.toggle('invisible')
+  updateCounts()
+}
 
 function updateCounts () {
   var totals = {
